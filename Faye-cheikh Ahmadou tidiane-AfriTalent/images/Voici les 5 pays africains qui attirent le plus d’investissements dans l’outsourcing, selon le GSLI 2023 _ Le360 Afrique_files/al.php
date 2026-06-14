af159ed0e7229db095db95ac86337447cc965209
mamoufaye39-pixel <mamoufaye39@gmail.com>
2026-06-14 22:14:36 +0000

    HTML : structure complète des 5 pages avec sections et contenu texte

---
+++ Faye-cheikh Ahmadou tidiane-AfriTalent/images/Voici les 5 pays africains qui attirent le plus d’investissements dans l’outsourcing, selon le GSLI 2023 _ Le360 Afrique_files/al.php
@@ -0,0 +1,3 @@
+var MAX_d72b5700 = '';
+MAX_d72b5700 += "<"+"div id=\'beacon_dbb3b44a63\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'https://pub.le360.ma/ads360/www/delivery/lg.php?bannerid=0&amp;campaignid=0&amp;zoneid=333&amp;loc=https%3A%2F%2Fafrique.le360.ma%2F&amp;cb=dbb3b44a63\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
+document.write(MAX_d72b5700);
---
+++ Faye-cheikh Ahmadou tidiane-AfriTalent/images/Voici les 5 pays africains qui attirent le plus d’investissements dans l’outsourcing, selon le GSLI 2023 _ Le360 Afrique_files/spcjs.php
@@ -0,0 +1,56 @@
+
+    if (typeof(OA_zones) != 'undefined') {
+        var OA_zoneids = '';
+        for (var zonename in OA_zones) OA_zoneids += escape(zonename+'=' + OA_zones[zonename] + "|");
+        OA_zoneids += '&amp;nz=1';
+    } else {
+        var OA_zoneids = escape('361|347|341|340|339|338|337|336|335|334|333|332|331|330|329|328|327|326|325|324|323|322|321|320|319|318|317|316|315|314|313|312');
+    }
+
+    if (typeof(OA_source) == 'undefined') { OA_source = ''; }
+    var OA_p=location.protocol=='https:'?'https://pub.le360.ma/ads360/www/delivery/spc.php':'http://pub.le360.ma/ads360/www/delivery/spc.php';
+    var OA_r=Math.floor(Math.random()*99999999);
+    OA_output = new Array();
+
+    var OA_spc="<"+"script type='text/javascript' ";
+    OA_spc+="src='"+OA_p+"?zones="+OA_zoneids;
+    OA_spc+="&amp;source="+escape(OA_source)+"&amp;r="+OA_r;
+    OA_spc+=(document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+
+    if (window.location) OA_spc+="&amp;loc="+escape(window.location);
+    if (document.referrer) OA_spc+="&amp;referer="+escape(document.referrer);
+    OA_spc+="'><"+"/script>";
+    document.write(OA_spc);
+
+    function OA_show(name) {
+        if (typeof(OA_output[name]) == 'undefined') {
+            return;
+        } else {
+            document.write(OA_output[name]);
+        }
+    }
+
+    function OA_showpop(name) {
+        zones = window.OA_zones ? window.OA_zones : false;
+        var zoneid = name;
+        if (typeof(window.OA_zones) != 'undefined') {
+            if (typeof(zones[name]) == 'undefined') {
+                return;
+            }
+            zoneid = zones[name];
+        }
+
+        OA_p=location.protocol=='https:'?'https://pub.le360.ma/ads360/www/delivery/apu.php':'http://pub.le360.ma/ads360/www/delivery/apu.php';
+
+        var OA_pop="<"+"script type='text/javascript' ";
+        OA_pop+="src='"+OA_p+"?zoneid="+zoneid;
+        OA_pop+="&amp;source="+escape(OA_source)+"&amp;r="+OA_r;
+        if (window.location) OA_pop+="&amp;loc="+escape(window.location);
+        if (document.referrer) OA_pop+="&amp;referer="+escape(document.referrer);
+        OA_pop+="'><"+"/script>";
+
+        document.write(OA_pop);
+    }
+var OA_fo = '';
+
+document.write(OA_fo);
